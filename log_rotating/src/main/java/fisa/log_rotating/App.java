d5ea6a4be5e3094c48e904ee23285dab9f348127
Jo-dv <whgjsgh11@gmail.com>
2026-02-04 09:48:14 +0900

    Initial commit

---
+++ log_rotating/src/main/java/fisa/log_rotating/App.java
@@ -0,0 +1,13 @@
+package fisa.log_rotating;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
